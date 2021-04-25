e5e0f09526698228591eea1232e8dadb2e37bfac
Sayed MD. Fahim Fahad <fahimfahad92@gmail.com>
2021-04-25 16:21:18 +0600

    complete homepage components and styling

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import './homepage.styles.scss';
+import Directory from '../../components/directory/directory.component';
+
+function HomePage() {
+  return (
+    <div className="homepage">
+        <Directory />
+    </div>
+  );
+}
+
+export default HomePage;
