028a90d7a8668dca7e7751b6f9b1a72e33d7efd5
Sayed MD. Fahim Fahad <fahimfahad92@gmail.com>
2021-04-27 11:19:12 +0600

    add header

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,19 @@
+import './header.styles.scss';
+import {Link} from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+
+function Header() {
+  return (
+       <div className="header">
+           <Link className="logo-container" to="/"><Logo className="logo" /></Link>
+            <div className="options">
+                <Link to="/shop" className="option">SHOP</Link>
+                <Link to="/shop" className="option">CONTACT</Link>
+            </div>
+
+        </div>
+  );
+}
+
+export default Header;
