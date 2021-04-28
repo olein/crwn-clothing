c5f6f8fe192f7558aaa4886d8f0255c1e68be89c
Sayed MD. Fahim Fahad <fahimfahad92@gmail.com>
2021-04-28 20:11:20 +0600

    sign in with email and password completed

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import './sign-in.styles.scss';
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -13,12 +13,22 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({
-            email:'',
-            password:''
-        })
+
+        const {email, password} = this.state;
+
+        try {
+            auth.signInWithEmailAndPassword(email, password);
+            this.setState({
+                email:'',
+                password:''
+            })
+            window.replace('/shop');
+        } catch(error) {
+            console.log(error);
+        }
+        
     }
 
     handleChange = event => {
